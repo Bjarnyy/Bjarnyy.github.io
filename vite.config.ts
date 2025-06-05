daa24036ee4257d8c2e21f019c9ff682548921fe
Bjarnyy <bjarny.destoop69@gmail.com>
2025-06-05 11:26:39 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,12 +11,12 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  // Do NOT include 'base' for GitHub user site like bjarnyy.github.io
 }));
